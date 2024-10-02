39a4254841b25e651f3b4b08cbc562fa7e1b8b03
dxmatheu <de.diego.mv@gmail.com>
2024-10-02 18:27:43 -0500

    feat: basic scrapping functionality. scraps the first 30 articles (with attributes: ranking, title, points, comments and id), creates a list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,13 @@ import it.skrape.core.*
 import it.skrape.fetcher.*
 
 
-class Article(var number: Int, var title: String, var points: Int = 0, var comments: Int = 0){
+class Article(var id: String, var ranking: Int, var title: String, var points: Int = 0, var comments: Int = 0){
     override fun toString(): String {
-        return "$number: $title, points: $points, comments: $comments"
+        return "$ranking: $title, points: $points, comments: $comments"
     }
 }
 fun main() {
-    var articleList = mutableListOf<Article>()
+    val articleList = mutableListOf<Article>()
     skrape(HttpFetcher) {
         request {
 
@@ -18,33 +18,41 @@ fun main() {
         }
         response {
             htmlDocument {
+                var articleCount = 0
                 ".athing"{
                     findAll{
                         forEach{
+                            val id = it.attribute("id")
                             val number = it.findFirst(".rank").text.removeSuffix(".").toInt()
                             val title = it.findFirst(".titleline a").text
-                            val tempArticle = Article(number, title)
-                            articleList.add(tempArticle)
+                            val tempArticle = Article(id, number, title)
+                            if (articleCount<30){
+                                articleList.add(tempArticle)
+                                articleCount++
+                            }
                         }
                     }
                 }
-                ".subtext"{
-                    findAll{
-                        forEach{
-                            val points = try {
-                                it.findFirst(".score").text.removeSuffix(" points").toInt()
-                            } catch (e: Exception){
-                                0
-                            }
-                            val comments = it.findLast("a").text
-                            println(comments)
-                        }
+
+                articleList.forEach { article ->
+                    val score = try {
+                        findFirst("#score_${article.id}").text.removeSuffix(" points").toInt()
+                    } catch (e: Exception){
+                        0
                     }
+                    article.points = score
+                    val comments = findSecond("a[href=\"item?id=${article.id}\"]").text
+                    if (comments.contains("comment"))
+                        article.comments = when{
+                            comments.endsWith(" comments") -> comments.removeSuffix(" comments").toInt()
+                            comments.endsWith(" comment") -> comments.removeSuffix(" comment").toInt()
+                            else -> 0
+                        }
                 }
             }
         }
         articleList.forEach{
-//            println(it)
+            println(it)
         }
     }
 }
